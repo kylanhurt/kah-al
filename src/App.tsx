e61800f133c513d8c2f91ceeeebe304fcf0991a8
Kylan Hurt <kylan.hurt@gmail.com>
2024-01-05 14:10:17 -0700

    About to transition again

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
-import React from "react";
-import { WagmiProvider } from "wagmi";
-import { config } from "./wagmi.ts";
-import "./App.css";
-import { SendForm } from "./SendForm.tsx";
-import { Connect } from "./Connect.tsx";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { useEffect, useState } from "react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { avalancheFuji } from "wagmi/chains";
+import { SendForm } from "./SendForm";
 
-function App() {
+// 1. Get projectID at https://cloud.walletconnect.com
+if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
+  throw new Error("You need to provide NEXT_PUBLIC_PROJECT_ID env variable");
+}
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+
+// 2. Configure wagmi client
+const chains = [avalancheFuji];
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ chains, projectId }),
+  publicClient,
+});
+
+// 3. Configure modal ethereum client
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
+
+export default function App() {
   return (
-    <WagmiProvider config={config}>
-      <Connect />
-      <SendForm />
-    </WagmiProvider>
+    <>
+      <WagmiConfig config={wagmiConfig}>
+        <SendForm />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
   );
 }
-
-export default App;
--- src/wagmi.ts
+++
@@ -1,9 +0,0 @@
-import { http, createConfig } from "wagmi";
-import { avalancheFuji } from "wagmi/chains";
-
-export const config = createConfig({
-  chains: [avalancheFuji],
-  transports: {
-    [avalancheFuji.id]: http(),
-  },
-});
