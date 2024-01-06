abe552c80b97977f551394e779704c2daa11f55e
Kylan Hurt <kylan.hurt@gmail.com>
2024-01-05 19:51:01 -0700

    Implement basic tests

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,4 @@
-import { ellipsizeCenter } from "./utils";
+import { ellipsizeCenter, getPreparedContractWrite } from "./utils";
 
 describe("ellipsizeCenter", () => {
   it("returns the original string if it's shorter than the max length", () => {
@@ -11,5 +11,28 @@ describe("ellipsizeCenter", () => {
 
   it("returns ellipsized string if string longer than max length", () => {
     expect(ellipsizeCenter("abcdefghijklmno", 8)).toEqual("abc...mno");
+  });
+});
+
+describe("getPreparedContractWrite", () => {
+  it("returns the correct object", () => {
+    expect(getPreparedContractWrite("0x123", "1")).toEqual({
+      address: "0x5425890298aed601595a70AB815c96711a31Bc65",
+      abi: [
+        {
+          constant: false,
+          inputs: [
+            { name: "_to", type: "address" },
+            { name: "_value", type: "uint256" },
+          ],
+          name: "transfer",
+          outputs: [{ name: "success", type: "bool" }],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ],
+      functionName: "transfer",
+      args: ["0x123", "1000000"],
+    });
   });
 });
